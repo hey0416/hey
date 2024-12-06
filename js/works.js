b902c9d69da287c0f1423c0f4515960bb5d2e7fc
hey0416 <hey0416@gmail.com>
2024-12-06 10:19:42 +0900

    Add files via upload

--- js/works.js
+++ js/works.js
@@ -34,3 +34,22 @@ timeline
     onStart: () => document.querySelector('.work3').classList.add('on'),
     onReverseComplete: () => document.querySelector('.work3').classList.remove('on'),
   });
+
+
+gsap.fromTo(".introduce .quarter-circle",
+  {
+    clipPath: "inset(0% % 100% 0% round 100% 100% 0 0)" // 시작 상태: 아무것도 보이지 않음
+  },
+  {
+    clipPath: "inset(0% 0% 0% 0% round 100% 100% 0 0)", // 끝 상태: 반원이 그려진 상태
+    duration: 0.7,                         // 애니메이션 지속 시간 1초로 설정
+    ease: "power2.out",                 // 부드러운 애니메이션 효과
+    scrollTrigger: {
+      trigger: ".introduce .quarter-circle", // 반원이 그려지는 위치 설정
+      start: "top center-=100px",         // 뷰포트 중앙에서 100px 아래에서 시작
+      end: "top+=100px center",           // 뷰포트 중앙에서 200px 아래에서 끝
+      scrub: false,                           // 스크롤과 애니메이션 동기화하지 않음 (즉시 애니메이션)
+      // markers: true                           // 디버깅 마커 표시 (개발 중 확인용)
+    }
+  }
+);
